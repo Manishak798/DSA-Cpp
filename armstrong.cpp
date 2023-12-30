33c5d39b8a1edbcc53c6747addf750a94aa3df9b
Manisha Kundnani <90680330+Manishak798@users.noreply.github.com>
2023-12-30 22:38:04 +0530

    Update armstrong.cpp

--- armstrong.cpp
+++ armstrong.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    long int num, cube = 0, sum = 0, rem, j, count = 0, m, k;
+    long long num, cube = 0, sum = 0, rem, j, count = 0, m, k;
     cout << "ENTER NUMBER YOU WANT TO CHECK WHEATHER IT IS A ARMSTRONG NUMBER OR NOT: ";
     cin >> num;
     k = num;
