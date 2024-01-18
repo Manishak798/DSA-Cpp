6ded2819f2de8650ba49f16b9388166a9cfb015c
Manisha Kundnani <90680330+Manishak798@users.noreply.github.com>
2024-01-18 22:26:53 +0530

    Update arraydeclaration.cpp

--- Arrays/arraydeclaration.cpp
+++ Arrays/arraydeclaration.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     // int A[5]; // array of size 5 without initializing
-    int A[4] = {1, 2, 3, 4};
+    int A[5] = {1, 2, 3, 4,5};
     cout << A[1];
     cout << endl
          << A[2];
