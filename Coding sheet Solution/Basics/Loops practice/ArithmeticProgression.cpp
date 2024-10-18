5e87fa4410915064ebd23c0879b00453470f8b3a
Manisha Kundnani <90680330+Manishak798@users.noreply.github.com>
2024-10-18 15:04:36 +0530

    Update ArithmeticProgression.cpp

--- Coding sheet Solution/Basics/Loops practice/ArithmeticProgression.cpp
+++ Coding sheet Solution/Basics/Loops practice/ArithmeticProgression.cpp
@@ -1,7 +1,7 @@
 /*Display Arithmetic Progression series
 5,10,15,20,25,30....so on..
 ap series has a common diffrence, a starting term, nth term ending term
-*/
+best */
 #include <bits/stdc++.h>
 using namespace std;
 int main()
