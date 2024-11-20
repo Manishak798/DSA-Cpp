34b3393119f3f25f2e1a7f6b46bef7390aba3ae1
Manisha Kundnani <90680330+Manishak798@users.noreply.github.com>
2024-11-20 20:23:40 +0530

    Create queue.cpp

---
+++ Cpp Basics/queue.cpp
@@ -0,0 +1,31 @@
+class MyQueue {
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        
+    }
+    
+    int pop() {
+        
+    }
+    
+    int peek() {
+        
+    }
+    
+    bool empty() {
+        
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
